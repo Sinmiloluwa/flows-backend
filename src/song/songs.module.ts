4335e5e7c6ec3868c5dd2fb645fdf98a51c00081
Sinmiloluwa <mofeoluwae@gmail.com>
2025-08-02 10:27:09 +0100

    created endpoint to get all songs

--- src/song/songs.module.ts
+++ src/song/songs.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
+import { Song } from './entities/song.entity';
 
 @Module({
-  providers: [SongsService],
+  imports: [JwtModule],
+  providers: [
+    SongsService,
+    {
+      provide: 'SONG_REPOSITORY',
+      useValue: Song,
+    }
+  ],
   controllers: [SongsController]
 })
 export class SongsModule {}
--- src/song/songs.service.ts
+++ src/song/songs.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Song } from './entities/song.entity';
+import { CreateSongDto } from './dtos';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+    constructor(
+        @Inject('SONG_REPOSITORY')
+        private songModel: typeof Song
+    ) {}
+
+    async addSong(songDto: CreateSongDto, userId?: string, artistId?: string): Promise<Song> {
+        console.log('Adding song:', songDto, 'for user:', userId, 'by artist:', artistId);
+        
+        if (!artistId) {
+            throw new Error('Artist ID is required');
+        }
+        
+        const songData = {
+            ...songDto,
+            artistId: artistId,
+            audio_url: songDto.audioFileUrl,
+            userId: userId
+        };
+        
+        const song = await this.songModel.create(songData);
+        return song;
+    }
+
+    async findAll(): Promise<Song[]> {
+        return this.songModel.findAll();
+    }
+}
