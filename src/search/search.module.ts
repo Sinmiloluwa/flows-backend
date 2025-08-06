09dd568f2b8736a895c7bb7a9bc718f0069eeb35
Sinmiloluwa <mofeoluwae@gmail.com>
2025-08-06 23:00:49 +0100

    added provider

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Playlist } from '../playlist/entities/playlist.entity';
+import { Song } from '../song/entities/song.entity';
+import { Artist } from '../artist/entities/artist.entity';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  providers: [SearchService],
+  imports: [
+    DatabaseModule,
+  ],
+  providers: [
+    {
+      provide: 'PLAYLIST_REPOSITORY',
+      useValue: Playlist,
+    },
+    {
+      provide: 'SONG_REPOSITORY', 
+      useValue: Song,
+    },
+    {
+      provide: 'ARTIST_REPOSITORY',
+      useValue: Artist,
+    },
+    SearchService
+  ],
   controllers: [SearchController]
 })
 export class SearchModule {}
