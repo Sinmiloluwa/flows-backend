bc3c1715ed9290fc5d7033b04019eca6dba250ac
Sinmiloluwa <mofeoluwae@gmail.com>
2025-08-06 23:01:05 +0100

    use op to search

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Playlist } from '../playlist/entities/playlist.entity';
+import { Song } from '../song/entities/song.entity';
+import { Artist } from '../artist/entities/artist.entity';
+import { Op } from 'sequelize';
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+  constructor(
+    @Inject('PLAYLIST_REPOSITORY') private readonly playlistModel: typeof Playlist,
+    @Inject('SONG_REPOSITORY') private readonly songModel: typeof Song,
+    @Inject('ARTIST_REPOSITORY') private readonly artistModel: typeof Artist
+  ) {}
+
+  async searchAll(query: string) {
+    const playlists = await this.playlistModel.findAll({
+      where: {
+        name: { [Op.iLike]: `%${query}%` },
+      },
+    });
+
+    const songs = await this.songModel.findAll({
+      where: {
+        title: { [Op.iLike]: `%${query}%` },
+      },
+    });
+
+    const artists = await this.artistModel.findAll({
+      where: {
+        name: { [Op.iLike]: `%${query}%` },
+      },
+    });
+
+    return { playlists, songs, artists };
+  }
+}
