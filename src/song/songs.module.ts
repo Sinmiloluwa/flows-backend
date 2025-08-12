a814782205aa81d7fe86b454e2de331a8229b83c
Sinmiloluwa <mofeoluwae@gmail.com>
2025-08-12 17:17:51 +0100

    added provider for recently played

--- src/song/songs.module.ts
+++ src/song/songs.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
 import { Song } from './entities/song.entity';
+import { RecentlyPlayed } from './entities/recently-played.entity';
 
 @Module({
   imports: [JwtModule],
@@ -11,7 +12,11 @@ import { Song } from './entities/song.entity';
     {
       provide: 'SONG_REPOSITORY',
       useValue: Song,
-    }
+    },
+    {
+      provide: 'RECENTLY_PLAYED_REPOSITORY',
+      useValue: RecentlyPlayed,
+    },
   ],
   controllers: [SongsController]
 })
